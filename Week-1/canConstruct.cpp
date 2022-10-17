936da584af8faf6f60e03d7392829d65c538aec3
Farid Ahmed <73395538+Farid4hmed@users.noreply.github.com>
2022-10-17 20:34:53 +0530

    Create canConstruct.cpp

---
+++ Week-1/canConstruct.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+       int arr[26] = {0};
+        for (int i = 0; i < magazine.length(); i++) {
+            arr[magazine[i] - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if(--arr[ransomNote[i]-'a'] < 0) {
+                return false;
+            }
+    }
+        return true;
+    }
+};
