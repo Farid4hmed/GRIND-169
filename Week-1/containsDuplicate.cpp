428735a789820ee1890308b0822c65d4a91369f5
Farid Ahmed <73395538+Farid4hmed@users.noreply.github.com>
2023-02-26 14:40:01 +0530

    Create containsDuplicate.cpp

---
+++ Week-1/containsDuplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> m;
+        for(auto it: nums){
+            m[it] += 1;
+            if(m[it] > 1)return true;
+        }
+        return false;
+    }
+};
